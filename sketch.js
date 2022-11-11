9db359e5aece04aa56a57646fbba53d14e2f8d67
Kylan Duncan <91110018+cloudydaiyz@users.noreply.github.com>
2022-11-11 17:51:31 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,13 +6,16 @@
 let density = "N@W$9876543210?!abc;:+=-,._ ";
 const len = density.length;
 
-let vid; // Video capture device to be defined in setup
+let media; // Video capture device or image to be defined in setup
 let asciiDiv; // HTML Div to be defined in setup
+let showPic = false;
 
 function preload() {
   // For loading images
   // Original image: https://www.google.com/search?q=dog&tbm=isch&hl=en&tbs=ic:trans%2Cisz:i&rlz=1C1EJFA_enUS1031US1031&sa=X&ved=0CAMQpwVqFwoTCLj69bTgnPsCFQAAAAAdAAAAABAH&biw=1903&bih=969#imgrc=YMNZPsFwjJB1HM
-  vid = loadImage("butterfly-dog.png");
+  if(showPic){
+    media = loadImage("spongebob.png");
+  }
 }
 
 function setup() {
@@ -21,22 +24,24 @@ function setup() {
   asciiDiv = createDiv();
   
   // Create a video capture
-  // vid = createCapture(VIDEO);
-  // vid.size(48, 48);
+  if(!showPic){
+    media = createCapture(VIDEO);
+    media.size(256, 256);
+  }
 }
 
 function draw() {
-  vid.loadPixels();
+  media.loadPixels();
   let asciiImage = "";
   
   // Goes through each row in the current frame's pixels
-  for(let row = 0; row < vid.width; row++){
-    for(let col = 0; col < vid.height; col++){
+  for(let row = 0; row < media.width; row++){
+    for(let col = 0; col < media.height; col++){
       // Extract the color information for the pixel at (row, col)
-      const pixelIndex = (col + row * vid.width) * 4;
-      const r = vid.pixels[pixelIndex];
-      const g = vid.pixels[pixelIndex + 1];
-      const b = vid.pixels[pixelIndex + 2];
+      const pixelIndex = (col + row * media.width) * 4;
+      const r = media.pixels[pixelIndex];
+      const g = media.pixels[pixelIndex + 1];
+      const b = media.pixels[pixelIndex + 2];
       
       // "brightness" of our pixel
       const avg = (r + g + b) / 3;
